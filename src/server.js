9494fc07b85d375d02a10146bb6ce0103e6f1988
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-11-30 11:28:31 +0530

    new commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import app from "./app.js"
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5000
+
 
 app.listen(PORT, () => {
   console.log("server running on port", PORT);
